e5fc4d866b0baa3cc70dd632b564dc60f3828769
Simone Manca <simone.manca.98@gmail.com>
2024-04-17 15:15:44 +0200

    esercizio2

---
+++ src/main/java/simonemanca/s7/l3/esercizio2/UserData.java
@@ -0,0 +1,20 @@
+package simonemanca.s7.l3.esercizio2;
+
+// Classe UserData
+public class UserData {
+    private String nomeCompleto;
+    private int eta;
+
+    public void getData(DataSource ds) {
+        nomeCompleto = ds.getNomeCompleto();
+        eta = ds.getEta();
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+}
