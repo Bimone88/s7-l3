9838fbe7ccd950b1a447536a8c51213c70c4f815
Simone Manca <simone.manca.98@gmail.com>
2024-04-17 15:12:31 +0200

    esercizio1

---
+++ src/main/java/simonemanca/s7/l3/Application.java
@@ -0,0 +1,13 @@
+package simonemanca.s7.l3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/simonemanca/s7/l3/esercizio1/DataSource.java
@@ -0,0 +1,6 @@
+package simonemanca.s7.l3.esercizio1;
+
+public interface DataSource {
+    String getNomeCompleto();
+    int getEta();
+}
